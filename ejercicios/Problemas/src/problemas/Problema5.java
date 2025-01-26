d593bcc4b4d399f6c849171cb239bd967ab30d67
Javier Vinan <javinan5@utpl.edu.ec>
2025-01-25 22:15:52 -0500

    cambios problema 5

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -11,4 +11,37 @@ package problemas;
  */
 public class Problema5 {
     
+    
+    public static void main(String[] args){
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] arreglo = new int [3][3];
+        
+        for(int i = 0; i < informacion.length; i++){
+            for(int j = 0; j < informacion[i].length; j++){
+                arreglo[i][j] = obtenerSuma(informacion[i][j],
+                        informacion2[i][j]);
+            }
+        }
+        obtenerReporte(arreglo);
+    }
+    public static int obtenerSuma(int a, int b){
+        int suma;
+        suma = a +b;
+        return suma;
+    }
+    public static void obtenerReporte(int[][] suma){
+        String mensaje = "";
+        
+        for(int i = 0; i < suma.length; i++){
+            for(int j = 0; j < suma[i].length; j++){
+                mensaje = String.format("%s%s\t", mensaje, suma[i][j]);
+            }
+            mensaje = String.format("%s\n", mensaje);
+        }
+        System.out.printf("%s\n", mensaje);
+    }
+    
+    
+    
 }
