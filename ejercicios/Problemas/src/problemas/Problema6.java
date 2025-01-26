77664179658ee7c0b6774671e3c4f96e0589b28f
Javier Vinan <javinan5@utpl.edu.ec>
2025-01-25 22:29:44 -0500

    cambios problema 6

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,39 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        double mediaA;
+        double desviacionE;
+
+        mediaA = obtenerMediaAritmetica(informacion);
+        desviacionE = obtenerDesviacionEstandar(informacion, mediaA);
+
+        System.out.printf("Media Artimetica: %.2f\n",
+                mediaA);
+        System.out.printf("Desviacion Estandar: %.2f\n",
+                desviacionE);
+    }
+    public static double obtenerMediaAritmetica(int[] a){
+        int suma = 0;
+        double media;
+        for(int i = 0; i < a.length; i++){
+            suma = suma + a[i];
+        }
+        media = suma/a.length;
+        
+        return media;
+    }
+    public static double obtenerDesviacionEstandar(int[] a, double b){
+        double suma = 0;
+        double desviacion;
+
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + Math.pow(a[i] - b, 2);
+        }
+
+        desviacion = Math.sqrt(suma / a.length);
+
+        return desviacion;
+    }
 }
