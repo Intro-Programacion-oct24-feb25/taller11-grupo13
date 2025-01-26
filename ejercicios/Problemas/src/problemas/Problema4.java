5477b4edf9dd57b248ca4cc8826a32682667f763
Javier Vinan <javinan5@utpl.edu.ec>
2025-01-25 19:41:36 -0500

    cambios problema 4

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package problemas;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -15,7 +15,50 @@ public class Problema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del cliente");
+        String nombres = entrada.nextLine();
+        System.out.println("Ingrese la cedula del cliente");
+        String cedula = entrada.nextLine();
+        System.out.println("Ingrese 1 para calcular el Valor de la Luz o 2 para"
+                + "calcular el Valor del Predio");
+        int indice = entrada.nextInt();
+        if(indice == 1){
+            calcularValorLuz(nombres, cedula);
+        }else{
+            if(indice == 2){
+                calcularPredio(nombres, cedula);
+            }
+        }
+    }
+    
+    public static void calcularValorLuz(String a, String b){
+        Scanner entrada = new Scanner(System.in);
+        int valorTotal;
+        System.out.println("Ingrese el valor del Kylobatio");
+        int valorKylo = entrada.nextInt();
+        System.out.println("Ingrese el numero del Kylobatio por mes");
+        int numeroKylo = entrada.nextInt();
+        valorTotal = valorKylo * numeroKylo;
+        
+        System.out.printf("Cliente %s con cedula %s debe cancelar el valor de $%d\n",
+                a,
+                b,
+                valorTotal);
+    }
+    public static void calcularPredio(String a, String b){
+        Scanner entrada = new Scanner(System.in);
+        double valorPredio;
+        System.out.println("Ingrese el valor del inmueble");
+        int valorInmueble = entrada.nextInt();
+        valorPredio = valorInmueble * 0.02;
+        
+        System.out.printf("Cliente %s con cedula %s tiene un bien inmueble"
+                + " valorado en $%d y tiene que pagar de predio $%.2f\n",
+                a,
+                b,
+                valorInmueble,
+                valorPredio);
     }
     
 }
